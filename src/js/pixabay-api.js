7dd4b6db3564717f67480ab97920164b389cb0ec
Silvia Zhdanska <silviaalbert1992@gmail.com>
2024-07-15 22:07:33 +0300

    key?

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,12 +3,24 @@ import axios from 'axios';
 const API_KEY = '44780398-b9dbe1b89370a8f5261d05d93';
 
 export async function getPicturesByQuery(query) {
-  const url = `https://pixabay.com/api/?key=${API_KEY}&q=${encodeURIComponent(
-    query
-  )}&image_type=photo&orientation=horizontal&safesearch=true`;
+  const params = {
+    key: '44784729-ebc9a0f5cc587c2700d41657d',
+    q: query,
+    imageType: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page: 1,
+    per_page: 15,
+  };
+
+  const url = 'https://pixabay.com/api/';
 
   try {
-    const response = await axios.get(url);
+    const response = await axios.get(url, { params });
+
+    if (response.status !== 200) {
+      throw new Error('Request failed with status ${response.status}');
+    }
 
     return response.data;
   } catch (error) {
